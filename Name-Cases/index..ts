ad6477c7b545407897990e604d059fbe8dfe75e4
kulsoom adnan <adnanirfan282@gmail.com>
2024-03-24 09:51:11 -0700

    new message

---
+++ Name-Cases/index..ts
@@ -0,0 +1,7 @@
+// we asssign a data in calling varaible .
+let calling : string = "mahnoor adnan"
+
+//then we apply a method on our data and the method is upper case , lower case and tittle case ....
+
+console.log("UPPER CASE:",calling.toUpperCase())
+console.log("LOWER CASE:",calling.toLowerCase())
