db60d390725307bb87eea364d967e57ba300c3d5
kulsoom adnan <adnanirfan282@gmail.com>
2024-03-24 09:26:22 -0700

    new changes

--- personal-message/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let personName = "Rimsha";
-console.log(`Hello ${personName} . would you like to learn some typescript?`);
--- personal-message/index.ts
+++
@@ -1,2 +0,0 @@
-let personName = "Rimsha"
-console.log(`Hello ${personName} . would you like to learn some typescript?` );
