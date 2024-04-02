afeff597169c6b4b85ab041de5c2da941fb46b31
kulsoom adnan <adnanirfan282@gmail.com>
2024-04-02 16:02:33 -0700

    initial chnages

---
+++ Exercise_03/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// famous person name or author 
+let famousPerson = "Quiad e Azam ";
+// quotaion 
+let Quote = "“I do not believe in taking the right decision, I take a decision and make it right.";
+// printing quation with the author .
+console.log(` Quaid-e-Azam Once said ,“I do not believe in taking the right decision, I take a decision and make it right."
+`);
