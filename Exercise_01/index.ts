149e99031558b9bcc88e433f0389707cae9b43a6
kulsoom adnan <adnanirfan282@gmail.com>
2024-04-02 15:58:22 -0700

    new changes

---
+++ Exercise_01/index.ts
@@ -0,0 +1,5 @@
+// store a name in variable 
+let person_Name = "kulsoom"
+// print the message to the variable store person
+
+console.log(`hello ${personName}. Would you like to learn typescript?`);
