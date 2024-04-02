3cc90f6cdd465cadbb6c8faca576128789f5b9ba
kulsoom adnan <adnanirfan282@gmail.com>
2024-04-02 16:03:30 -0700

    new changes

---
+++ Exercise_04/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// famous person name or author 
+let famous_Person = "Quiad e Azam ";
+// quotaion 
+let message = "“I do not believe in taking the right decision, I take a decision and make it right.";
+// printing our message 
+console.log(`${famous_Person} Once said,${message}`);
