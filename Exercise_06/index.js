8e14a102a672eb435e35085d4d3f5bd91dce3435
kulsoom adnan <adnanirfan282@gmail.com>
2024-04-02 17:09:23 -0700

    initial commit

---
+++ Exercise_06/index.js
@@ -0,0 +1,9 @@
+"use strict";
+//include some whitespace characters at the beginning and end of the name. 
+// store name in variable
+let a = "\t\n 'kulsoom'\n\t";
+console.log(a);
+let b = "\t 'RIMSHA' \t";
+console.log(b);
+let g = " \t\n `MAHNOOR` \n\t ";
+console.log(g);
